625bd97662a9a547f3c9c7223683e4a54e14fdd7
Никита Богов <v.zotman@yandex.ru>
2021-06-30 22:01:19 +0300

    Revert "local test"
    This reverts commit d76031737f25a15e7f5a69726d27ba7a045e8b13.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello!");
         }
     }
 }
